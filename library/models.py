2f6e52bc9a193b74c3915d8d6104cbefdf81d4bb
msadour <sadour.mehdi@gmail.com>
2020-01-16 16:14:41 +0100

    integrate postgresql

--- library/models.py
+++ library/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from datetime import datetime
-
+#
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -42,8 +42,8 @@ class Settings(SingletonModel):
     Class Settings
     """
     theme = models.CharField(max_length=300, default='dark')
-
-
+#
+#
 class PublishManager(models.Manager):
     """
     Objects manager worked with dates published
